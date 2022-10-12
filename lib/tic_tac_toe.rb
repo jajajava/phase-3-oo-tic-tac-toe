9fb37fddd1e8e4b46dcf2582e1171e943342e63b
David <davidstepanov@yahoo.com>
2022-10-12 13:06:57 -0400

    commit tree c47e46a540e4e6a5a95b0af63d50e213481efcb6

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,17 @@
+# require "pry"
+
+
+# class Tic_tac_toe
+#     attr_accessor :input, :board
+    
+#     @board = []
+
+#     def inputter (input)
+#         self.board.push(input)
+#         binding.pry
+
+#     end
+# end
+
+# tic_tac_toe = Tic_tac_toe.new
+# tic_tac_toe.inputter(1)
